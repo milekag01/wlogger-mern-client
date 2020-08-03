5d9d2e00b51b0c96482e41aaea12a6cf0a485146
milek agrawal <milekag01@gmail.com>
2020-08-03 11:29:18 +0530

    add google analytics

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d1829bd6bd18079bff18";
+/******/ 	var hotCurrentHash = "181cf0a3962237cd7555";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,21 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-
+import getConfig from 'next/config';
+const { publicRuntimeConfig } = getConfig();
 class MyDocument extends Document {
+
+  setGoogleTags() {
+    if (!publicRuntimeConfig.PRODUCTION) {
+      return {
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'UA-174284863-1');
+        `
+      };
+    }
+  }
   render() {
     return (
       <Html lang="en">
@@ -12,7 +27,14 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css"
           />
+
+          <React.Fragment>
+            <script dangerouslySetInnerHTML={this.setGoogleTags()} />
+          </React.Fragment>
+
           <link rel="stylesheet" href="/static/css/styles.css" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-174284863-1"></script>
+
         </Head>
         <body>
           <Main />
